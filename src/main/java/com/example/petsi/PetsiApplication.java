d851d2e09ba39b33a7dd3426c3152577a282daf5
djeu1116 <rladbgus781@gmail.com>
2025-05-24 20:46:16 +0900

    chore1 : 백엔드 초기 환경 세팅

---
+++ src/main/java/com/example/petsi/PetsiApplication.java
@@ -0,0 +1,13 @@
+package com.example.petsi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetsiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetsiApplication.class, args);
+	}
+
+}
